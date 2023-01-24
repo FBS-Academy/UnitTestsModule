c6b6a6faca7f2a78d9d4068e03b16ae271aa224c
FBS Academy <fbsacademy@outlook.com>
2023-01-24 20:02:26 +0000

    Installing and configuring Moq

--- UnitTestsModule.ApplicationTests/StudentServiceTests.cs
+++ UnitTestsModule.ApplicationTests/StudentServiceTests.cs
@@ -1,13 +1,38 @@
+using FluentAssertions;
+using Moq;
+using System.Threading.Tasks;
+using UnitTestsModule.Application;
+using UnitTestsModule.Application.Repositories;
+using UnitTestsModule.Domain;
 using Xunit;
 
 namespace UnitTestsModule.ApplicationTests
 {
     public class StudentServiceTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly Mock<IStudentRepository> studentRepository;
+        private readonly StudentService studentService;
 
+        public StudentServiceTests()
+        {
+            this.studentRepository = new Mock<IStudentRepository>();
+            this.studentService = new StudentService(this.studentRepository.Object);
+        }
+
+        [Fact]
+        public async Task InsertAsync_CorrectStudent_ShouldReturnSuccess()
+        {
+            // Arrange
+            this.studentRepository.Setup(repository => repository.InsertOrUpdateAsync(It.IsAny<Student>()))
+                .ReturnsAsync(1);
+
+            var student = new Student("Name");
+
+            // Act
+            var result = await this.studentService.InsertAsync(student);
+
+            // Assert
+            result.Should().Be(1);
         }
     }
 }
