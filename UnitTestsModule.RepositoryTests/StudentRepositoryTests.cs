64cc882ee075dd3891c196d1bfc0b690bbdc6ab1
FBS Academy <fbsacademy@outlook.com>
2023-01-29 16:40:53 +0000

    Using the InMemoryDataBaseContext

---
+++ UnitTestsModule.RepositoryTests/StudentRepositoryTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using System.Threading.Tasks;
+using UnitTestsModule.Domain;
+using UnitTestsModule.Repository;
+using Xunit;
+
+namespace UnitTestsModule.RepositoryTests
+{
+    public class StudentRepositoryTests
+    {
+        private readonly InMemoryDataBaseContext dbContext;
+        private readonly StudentRepository repository;
+
+        public StudentRepositoryTests()
+        {
+            this.dbContext = new InMemoryDataBaseContext();
+            this.repository = new StudentRepository(this.dbContext);
+        }
+
+        [Fact]
+        public async Task InsertOrUpdateAsync_CorrectStudent_ShouldReturnSuccess()
+        {
+            // Arrange
+            var student = new Student("Name");
+
+            // Act
+            var result = await this.repository.InsertOrUpdateAsync(student);
+
+            // Assert
+            result.Should().Be(1);
+        }
+    }
+}
