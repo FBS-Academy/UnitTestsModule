b42ffbb9874cd878fdd259cdc8c4f7364280103c
FBS Academy <fbsacademy@outlook.com>
2023-01-29 16:27:08 +0000

    Added InMemoryDataBaseContext

---
+++ UnitTestsModule.RepositoryTests/InMemoryDataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using UnitTestsModule.Repository.Configs;
+
+namespace UnitTestsModule.RepositoryTests
+{
+    public class InMemoryDataBaseContext : UnitTestsDbContext
+    {
+        private static readonly ILoggerFactory loggerDebugFactory = LoggerFactory.Create(builder => { builder.AddDebug(); });
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .EnableDetailedErrors()
+                .EnableSensitiveDataLogging()
+                .UseLoggerFactory(loggerDebugFactory)
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString());
+        }
+    }
+}
